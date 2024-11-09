60a4fcccf3cf28afe11e4f7cc1d90590bc20d4f2
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2024-11-08 20:19:54 -0600

    Moved content from template.html to index.js.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import "./styles.css";
+import hamburger from "./hamburger.webp";
 
 console.log("Hello customer!");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const content = document.getElementById("content");
+
+  // Add new text:
+  const newText = `
+    <h1 id="featured-item">Try our new juicy burger for $14.99!</h1>    
+    `;
+  content.innerHTML += newText;
+
+  // Add image:
+  const image = document.createElement("img");
+  image.src = hamburger;
+  image.alt = "hamburger image";
+  content.appendChild(image);
+});
