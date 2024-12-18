8e6976afda02a7ea0e2ab80a8312dac9a48fab41
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2024-12-17 18:35:21 -0600

    Added more menu items

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,9 @@
 import cheeseburger from "./images/cheeseburger.webp";
-import garlicwafflefries from "./images/garlicwafflefries.jpg";
-import mermaidlemonade from "./images/mermaidlemonade.jpg";
+import hugeBurger from "./images/hugeburger.jpg";
+import kidsBurger from "./images/kidsburger.jpg";
+import lavaBrownie from "./images/lavabrownie.webp";
+import garlicWaffleFries from "./images/garlicwafflefries.jpg";
+import mermaidLemonade from "./images/mermaidlemonade.jpg";
 
 export function loadMenu() {
   const content = document.getElementById("content");
@@ -13,8 +16,11 @@ export function loadMenu() {
   // Menu items data:
   const menuItems = [
     { title: "Classic Cheeseburger", image: cheeseburger },
-    { title: "Garlic Waffle Fries", image: garlicwafflefries },
-    { title: "Signature Mermaid Lemonade", image: mermaidlemonade },
+    { title: "Big Ol' Burger", image: hugeBurger },
+    { title: "Kid's Burger", image: kidsBurger },
+    { title: "Garlic Waffle Fries", image: garlicWaffleFries },
+    { title: "Lava Brownie", image: lavaBrownie },
+    { title: "Signature Mermaid Lemonade", image: mermaidLemonade },
   ];
 
   // Generate HTML for menu items:
